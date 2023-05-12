17c2b2814da1732a03e3775e2ec1bf56726e88e3
jalonso <jalonso-colladaa01@educantabria.es>
2023-05-12 20:05:02 +0200

    Añadir comentarios al archivo de Java

---
+++ ProyectoGit.java
@@ -0,0 +1,16 @@
+
+package proyectogit;
+
+/**
+ *
+ * @author jalonso
+ */
+public class ProyectoGit {
+
+    
+    public static void main(String[] args) {
+      System.out.println("proyecto para experimentar con ramas de Git");
+   //COMENTARIO DE PRUEBAS
+ }
+    
+}
