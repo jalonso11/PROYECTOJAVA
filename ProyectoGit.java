2b94f03508195a7b11a7a1fe3250c48a2fcbdf36
jalonso <jalonso-colladaa01@educantabria.es>
2023-05-12 20:08:12 +0200

    Comentarios añadidos solo en la rama pruebas

--- ProyectoGit.java
+++ ProyectoGit.java
@@ -11,6 +11,7 @@ public class ProyectoGit {
     public static void main(String[] args) {
       System.out.println("proyecto para experimentar con ramas de Git");
    //COMENTARIO DE PRUEBAS
+   //COMENTARIO EXCLUSIVO DE LA RAMA PRUEBAS
  }
     
 }
