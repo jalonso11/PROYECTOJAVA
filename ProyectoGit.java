948b1a949405bce73cd0504c7d072896a44b2486
jalonso <jalonso-colladaa01@educantabria.es>
2023-05-12 20:12:00 +0200

    Comentar main

--- ProyectoGit.java
+++ ProyectoGit.java
@@ -11,6 +11,7 @@ public class ProyectoGit {
     public static void main(String[] args) {
       System.out.println("proyecto para experimentar con ramas de Git");
    //COMENTARIO DE PRUEBAS
+    //COMENTARIO INTRODUCIDO EN MAIN
  }
     
 }
